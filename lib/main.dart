66a6c6273e388216097716d5028a6f5067cba65e
CarlosMS12 <carlosmagallanes207@gmail.com>
2024-07-17 22:58:31 -0500

    agregando input y titulo home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_movie_tbm/pages/home_page/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,15 +9,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'App Movie',
+      home: HomePage(),
     );
   }
 }
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -5,6 +5,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 20, left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                      width: 10,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  suffixIcon: Icon(Icons.search, color: Color(0xff004cd9)),
+                  hintText: 'Search',
+                  hintStyle: TextStyle(color: Color(0xff979797)),
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+                ),
+              ),
+              SizedBox(height: 20),
+              Text("Now Playing",
+                  style: TextStyle(color: Colors.white, fontSize: 25)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
